738349f1e88c5bb3ce7b5039c1c4f0711a873739
6396dhruv <6396dhruv@gmail.com>
2025-08-14 12:19:34 +0530

    new code

---
+++ Project1/Main.py
@@ -0,0 +1,30 @@
+# step 1 - import the random module
+import random
+
+# create subjects
+subjects = ["salman khan", "virat kohli", "rohit sharma", "A group of monkey", "prime minister modi", "auto rikshaw driver", "jaat"]
+
+actions = ["launches", "cancels", "eats", "dances with", "declares war on", "orders", "celebrates"]
+
+places_of_actions = ["at red fort", "plate of samosa", "inside parliamememt", "during ipl match", "at india gate", "at ganga ghat",
+"local train"]
+
+
+# start the headline generation loop
+# strip() - l strip and r strip for left and right space
+
+while True:
+    subject =  random.choice(subjects)
+    action = random.choice(actions)
+    places = random.choice(places_of_actions)
+
+
+    headline = f"Breaking News: {subject} {action} {places}"
+
+    print("\n" + headline)
+    user = input("\n Do you want another headline? (yes/no).").strip().lower()
+    if user == "no":
+        break
+
+# print a goodbye message
+print("\n Thanks for using the fake news headline generator have fun!")
---
+++ Project2/main.py
@@ -0,0 +1,68 @@
+def show_history():
+    file = open("history.txt", "r")
+    lines = file.readlines()
+    if len(lines) ==0:
+        print("No history found!")
+    else:
+        for line in reversed(lines):
+            print(line.strip())
+    file.close()
+
+def clear_history():
+    file = open("history.txt", "w")
+    file.close()
+    print("History cleared.")
+
+def save_history(equation, result):
+    file = open("history.txt", 'a')
+    file.write(equation + "=" + str(result) + "\n")
+    file.close()
+
+def calculate(user_input):
+    parts = user_input.split()
+    if len(parts)!= 3:
+        print("Invalid input. use format number operator number (eg- 6+6)")
+        return
+    num1 = float(parts[0])
+    op = parts[1]
+    num2 = float(parts[2])
+
+    if op == "+":
+        result = num1 + num2
+    elif op == "-":
+        result = num1 - num2
+    elif op == "*":
+        result = num1 * num2
+    elif op == "/":
+        if num2 == 0:
+            print("You can not divide with zero")
+            return
+        result = num1 / num2
+    else:
+        print("Invalid operator.")
+        return
+    
+    if int(result) == result:
+        result = int(result)
+    print("Result: ", result)
+    save_history(user_input, result)
+
+def main():
+    print("Simple Calculator (type history, clear or exit)")
+    while True:
+        user_input = input("Enter calculations (+ - * /) or command (history, clear and exit) = ")
+        if user_input == "exit":
+            print("Good Bye!")
+            break
+        elif user_input == "history":
+            show_history()
+        elif user_input == "clear":
+            clear_history()
+        else:
+            calculate(user_input)
+
+main()
+
+
+ 
+
